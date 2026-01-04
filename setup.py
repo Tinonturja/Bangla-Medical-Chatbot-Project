da0ec6e217db79d1331cc7711ac8a6952ac5a658
Tinonturja <tinonturja@gmail.com>
2026-01-04 22:19:44 +0600

    project setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Bangla MedicalChatbot Paper',
+    version = '0.0.0',
+    author = 'Tinon Turja Majumder',
+    author_email= 'tinonturja@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
